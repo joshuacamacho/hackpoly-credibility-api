44398292efe6abfaf35470495ded8cf5afcda5bc
Cristian <cavinalopez@cpp.edu>
2017-02-11 17:39:47 -0800

    added file reader to app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const fs = require('fs');
+
 app.use(bodyParser.urlencoded({ extended: true}));
 // declare static content
 app.use(express.static('views'));
@@ -10,7 +12,16 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
-      response.sendFile(__dirname + '/public/index.html');
+    response.sendFile(__dirname + '/public/index.html');
+});
+
+// All user data being requested from the API
+app.get('/users', (request, response) => {
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', (err, data) => {
+        if (err) { console.log('oops, something went wrong ' + err )};
+        console.log( data );
+        response.end( data );
+    });
 });
 
 app.listen(3000, () => {
